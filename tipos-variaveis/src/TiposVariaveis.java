83eae36404fc6f17056d9aa5946d94ef28f45d55
Marcus Sandes <marcus.sandesf@gmail.com>
2024-09-26 07:34:49 -0300

    update

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estudem a classe String que representa texto na aplicação
+
+        String meuNome = "Marcus Sandes";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
